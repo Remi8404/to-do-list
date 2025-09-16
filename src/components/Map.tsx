717b046d4858f6ffa8d579d138c26ea51d6a5d61
Remi8404 <remi.aerts04@gmail.com>
2025-09-16 12:21:43 +0200

    Update Map.tsx -> change pin upon isDone state

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,30 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import useToDos from "../hooks/hook";
+import * as L from "leaflet";
 
 interface ToDo {
   name: string;
+  isDone: boolean;
   lat?: number;
   lng?: number;
 }
+var greenIcon = L.icon({
+  iconUrl: "gps_done.png",
+  iconSize: [38, 38], // size of the icon
+  iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
+  popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
+});
+var redIcon = L.icon({
+  iconUrl: "gps_notdone.png",
+  iconSize: [38, 38], // size of the icon
+  iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
+  popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
+});
 
 const Map = () => {
-  // La position doit être un tableau avec deux valeurs : latitude et longitude
   const center: [number, number] = [48.857739, 2.294844];
-  const todos: ToDo[] = useToDos(); // Typage des tâches avec `ToDo[]`
+  const todos: ToDo[] = useToDos();
 
   return (
     <div style={{ height: "calc(100vh - 168px)" }}>
@@ -25,7 +38,11 @@ const Map = () => {
           const position: [number, number] = [task.lat ?? 0, task.lng ?? 0];
           if (position[0] !== 0 || position[1] !== 0) {
             return (
-              <Marker key={index} position={position}>
+              <Marker
+                key={index}
+                position={position}
+                icon={task.isDone ? greenIcon : redIcon}
+              >
                 <Popup>{task.name}</Popup>
               </Marker>
             );
